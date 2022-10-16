aa7b742dc3137a963a0a6093e351f3582ebf51c4
Malhar Bhuptani <mbhuptani@ipdanalytics.onmicrosoft.com>
2022-10-17 00:56:36 +0530

    added getJobById and getCompanyById schema and resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,6 +3,8 @@ import { Company, Job } from "./db.js";
 export const resolvers = {
     Query: {
         jobs: () => Job.findAll(),
+        job: (_root, { id }) => Job.findById(id),
+        company: (_root, { id }) => Company.findById(id),
     },
 
     Job: {
