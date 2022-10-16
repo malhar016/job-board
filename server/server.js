8b4a99f002fd15453a746418b66f8b7677c70ab7
Malhar Bhuptani <mbhuptani@ipdanalytics.onmicrosoft.com>
2022-10-16 17:51:54 +0530

    added sample schema and resolver with apollo middleware for express

--- server/server.js
+++ server/server.js
@@ -1,31 +1,48 @@
-import cors from 'cors';
-import express from 'express';
-import { expressjwt } from 'express-jwt';
-import jwt from 'jsonwebtoken';
-import { User } from './db.js';
-import { ApolloServer } from 'apollo-server-express';
+import cors from "cors";
+import express from "express";
+import { expressjwt } from "express-jwt";
+import jwt from "jsonwebtoken";
+import { User } from "./db.js";
+import { readFile } from "fs/promises";
+import { resolvers } from "./resolvers.js";
+import { ApolloServer } from "apollo-server-express";
 
 const PORT = 9000;
-const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+const JWT_SECRET = Buffer.from("Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt", "base64");
 
 const app = express();
-app.use(cors(), express.json(), expressjwt({
-  algorithms: ['HS256'],
-  credentialsRequired: false,
-  secret: JWT_SECRET,
-}));
+app.use(
+  cors(),
+  express.json(),
+  expressjwt({
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+    secret: JWT_SECRET,
+  })
+);
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne((user) => user.email === email);
   if (user && user.password === password) {
     const token = jwt.sign({ sub: user.id }, JWT_SECRET);
-    res.json({ token });  
+    res.json({ token });
   } else {
     res.sendStatus(401);
   }
 });
 
+const typeDefs = await readFile("./schema.graphql", "utf-8");
+console.log("typeDefs: ", typeDefs);
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+await apolloServer.start();
+
+apolloServer.applyMiddleware({ app, path: "/graphql" });
+
 app.listen({ port: PORT }, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
+  console.log(
+    `GraphQL Server running on port http://localhost:${PORT}/graphql`
+  );
 });
